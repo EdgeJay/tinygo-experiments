42a704145575ccb0e92aaf32d63fd2b0e9f444e1
EdgeJay <hjwu85@gmail.com>
2026-05-24 12:56:02 +0800

    clear var first

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-DEVICE=$(dmesg | tail -50 | grep -Po "^(?:\[\s*\d+\.\d+\])\s\s(?:sd[a-z]\:\s)\K(sd[a-z]\d)")
+unset DEVICE
+DEVICE=$(dmesg | tail -30 | grep -Po "^(?:\[\s*\d+\.\d+\])\s\s(?:sd[a-z]\:\s)\K(sd[a-z]\d)")
 echo "Mounting device: /dev/$DEVICE"
 sudo mount /dev/$DEVICE /mnt/rp2040
