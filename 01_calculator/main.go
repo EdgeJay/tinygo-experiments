159e3f36b641b344ae3342e94fb2c868c7dc5e3f
EdgeJay <hjwu85@gmail.com>
2026-05-24 12:56:56 +0800

    test output all possible gopher characters

--- 01_calculator/main.go
+++ 01_calculator/main.go
@@ -8,7 +8,7 @@ import (
 	"tinygo.org/x/drivers"
 	"tinygo.org/x/drivers/ssd1306"
 	"tinygo.org/x/tinyfont"
-	"tinygo.org/x/tinyfont/freemono"
+	"tinygo.org/x/tinyfont/gophers"
 )
 
 func main() {
@@ -30,7 +30,19 @@ func main() {
 
 	white := color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xFF}
 
-	tinyfont.WriteLine(display, &freemono.Bold9pt7b, 5, 12, "CALCULATOR", white)
-	// tinyfont.WriteLine(display, &gophers.Regular32pt, 5, 50, "ABCEF", white)
-	display.Display()
+	// tinyfont.WriteLine(display, &freemono.Bold9pt7b, 5, 12, "CALCULATOR", white)
+
+	// create int var that stores ASCII value of A to Z (65 to 90) characters
+	asciiVal := 65
+
+	for {
+		display.ClearDisplay()
+		tinyfont.WriteLine(display, &gophers.Regular32pt, 5, 50, string(rune(asciiVal)), white)
+		asciiVal++
+		if asciiVal > 90 {
+			asciiVal = 65
+		}
+		display.Display()
+		time.Sleep(1000 * time.Millisecond)
+	}
 }
